789698e2f37d8d7cd653e7e17e6de74635a859f6
wdkrbturn <106270639+wdkrbturn@users.noreply.github.com>
2025-04-19 03:38:01 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,11 +2,11 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Kali Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '+49 1573 5612677', //Your number
+  botOwner: 'Kerem', // Your name
+  ownerNumber: '4915735612677', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "This is a bot for kali the ghost 🤫.",
   version: "1.0.0",
 };
 
